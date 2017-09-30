f7afc26b612988a428fc0f8bc8062588b87e953d
atg.abhishek <atg.abhishek@gmail.com>
2017-09-30 18:39:50 -0400

    test endpoint

--- ubuntu_server.py
+++ ubuntu_server.py
@@ -1,10 +1,25 @@
-from flask import Flask 
+from flask import Flask, request, jsonify
+from chatbot import chatbot
+from pprint import pprint 
+import os
+c = chatbot.Chatbot()
+c.main()
 
 ubuntu_app = Flask(__name__)
 
 @ubuntu_app.route('/')
 def hello():
     return "Welcome to the ubuntu server"
+
+@ubuntu_app.route('/get_answer', methods=['POST'])
+def get_answer():
+    data = request.get_json()
+    message = data['message']
+    # os.system("python3 main.py --test daemon")
+
+
+    # res = c.predictOnQuery(message)
+    return jsonify({"result" : message} )
 
 if __name__ == "__main__":
     ubuntu_app.run(host='0.0.0.0',debug=True)
