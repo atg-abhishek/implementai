6e33f8cdd58adea9e314d89ee96fcdda05bbd88f
atg.abhishek <atg.abhishek@gmail.com>
2017-09-30 10:50:41 -0400

    setting up mongodb

--- server.py
+++ server.py
@@ -1,10 +1,22 @@
 from flask import Flask 
+from pymongo import MongoClient
+import os
+
+client = MongoClient(os.environ['MONGODB_URI'])
+db = client[os.environ['MONGO_DB_NAME']]
+
+users = db['users']
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Welcome to the server for the Implement AI hackathon"
+
+@app.route('/db/add_user')
+def add_user():
+    users.insert_one({'user_id' : "hello"})
+    return "done"
 
 if __name__ == "__main__":
     app.run(debug=True)
