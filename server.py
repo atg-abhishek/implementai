44af86a229a5f24a86e56919075b4f0d0631d164
atg.abhishek <atg.abhishek@gmail.com>
2017-09-30 11:31:56 -0400

    prelim code for the bot

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from pymongo import MongoClient
-import os
+import os, json, requests
 
 client = MongoClient(os.environ['MONGODB_URI'])
 db = client[os.environ['MONGO_DB_NAME']]
@@ -21,6 +21,51 @@ def add_user():
 @app.route('/facebook_incoming')
 def facebook_incoming():
     return request.args['hub.challenge']
+
+@app.route('/facebook_incoming', methods=['POST'])
+def webhook():
+    # CITATION :  version based on https://github.com/hartleybrody/fb-messenger-bot/blob/master/app.py
+    data = request.get_json()
+    if data['object'] == 'page':
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):  # someone sent us a message
+
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["message"]["text"]  # the message's text
+
+                    send_message(sender_id, "roger that!")
+
+                if messaging_event.get("delivery"):  # delivery confirmation
+                    pass
+
+                if messaging_event.get("optin"):  # optin confirmation
+                    pass
+
+                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    pass
+
+    return "ok", 200
+
+def send_message(recipient_id, message_text):
+    # CITATION :  version based on https://github.com/hartleybrody/fb-messenger-bot/blob/master/app.py
+    params = {
+        "access_token": os.environ["FB_PAGE_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
