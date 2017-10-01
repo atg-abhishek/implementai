c8614ec61b52309c10e99ab1abeac5c5808710d4
atg.abhishek <atg.abhishek@gmail.com>
2017-10-01 13:34:24 -0400

    added stub code for the function

--- other_server.py
+++ other_server.py
@@ -8,5 +8,15 @@ ubuntu_app = Flask(__name__)
 def hello():
     return "Welcome to the other server"
 
+@ubuntu_app.route('/get_answer', methods=['POST'])
+def get_answer():
+    data = request.get_json()
+    incoming_message = data['message']
+    with open('input.test','w') as outfile:
+        outfile.write(incoming_message)
+    #TODO: interface with the function 
+    res = ""
+    return jsonify({"result" : res})
+
 if __name__ == "__main__":
     ubuntu_app.run(host='0.0.0.0',debug=True)
